4013205a181524efff188878ec8835ef9b4ab897
cz.lee <cz.lee@chelpis.com>
2021-10-24 03:34:40 +0800

    feat: styling with chakra-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import { useActor } from '@xstate/react'
 import { mainService } from './main.machine'
+import { Container, Flex, Button, Heading, Badge, ScaleFade, Text } from '@chakra-ui/react'
+import { match } from 'ts-pattern'
 
 function App() {
   const [state, send] = useActor(mainService)
 
   return (
-    <div className='App'>
-      <button onClick={() => send({ type: "PING" })}>ping</button>
-      <div>status: {state.value}</div>
-      {state.matches('received') && <div>pong!</div>}
-    </div>
+    <Container maxWidth="container.sm">
+      <Flex direction="column" alignItems="center">
+        <Button marginBlockStart={8} disabled={!state.matches('idle')} onClick={() => send({ type: "PING" })}>ping</Button>
+        <Heading size="sm" marginBlockStart={4}>status:
+          <Badge marginInlineStart={2} colorScheme={match(state).when((state) => state.matches('idle'), () => 'gray').when((state) => state.matches('waitingForResponse'), () => 'blue').when((state) => state.matches('received'), () => 'green').otherwise(() => 'gray')}>{state.value}</Badge>
+        </Heading>
+        <ScaleFade in={state.matches('received')} unmountOnExit initialScale={0.5}>
+          {<Text marginBlockStart={4} fontSize="xl" fontWeight="bold">Pong!</Text>}
+        </ScaleFade>
+      </Flex>
+    </Container>
   )
 }
 
